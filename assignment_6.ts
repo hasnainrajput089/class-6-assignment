8669819b6dbf403a9b514218d9aad13aa1fc268c
hasnainrajput089 <159238536+hasnainrajput089@users.noreply.github.com>
2024-03-14 19:57:57 -0700

    Add files via upload

---
+++ assignment_6.ts
@@ -0,0 +1,44 @@
+//objects =>{}=>{key:value}:
+
+let car = {
+    name : "fortuner",
+    color : "black",
+    model : 2024,
+    price : "80 lacs",
+    automatic : true
+
+};
+
+//object in array:
+
+let student_result = {
+    full_Name : "M.Hasnain",
+    class : 10,
+    rollName :11,
+    email  : "hasnainrajput.089@gmail.com",
+    Grade :'A',
+    Ranked :null,
+    subject :['biology', 'math','english','chemistry','physics','etc.'] ,
+    subject_marks : {
+        biology : 55,
+        math :60,
+        english :44,
+        chemistry :70,
+        physics :71,
+
+    },
+    total_number :419,
+    ispass :true,
+};
+
+//car print:
+console.log(car);
+console.log(car.name);
+
+//student_result print:
+console.log(student_result);
+console.log(student_result.subject[2]);
+console.log(student_result.subject_marks.math);
+
+
+
