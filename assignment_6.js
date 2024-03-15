b7d1675edafd6ed04419066c96eb0ea928dc9f8c
hasnainrajput089 <159238536+hasnainrajput089@users.noreply.github.com>
2024-03-14 19:57:28 -0700

    Add files via upload

---
+++ assignment_6.js
@@ -0,0 +1,29 @@
+//objects =>{}=>{key:value}:
+var car = {
+    name: "fortuner",
+    color: "black",
+    model: 2024,
+    price: "80 lacs",
+    automatic: true
+};
+//object in array:
+var student_result = {
+    full_Name: "M.Hasnain",
+    class: 10,
+    rollName: 11,
+    email: "hasnainrajput.089@gmail.com",
+    Grade: 'A',
+    Ranked: null,
+    subject: ['biology', 'math', 'english', 'chemistry', 'physics', 'etc.'],
+    subject_marks: {
+        biology: 55,
+        math: 60,
+        english: 44,
+        chemistry: 70,
+        physics: 71,
+    },
+    total_number: 419,
+    ispass: true,
+};
+//car print:
+console.log(car);
